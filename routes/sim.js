77cec1cc608733d910a2c84e10a5112b69f6d5c6
Steve Troxel <troxel@perlworks.com>
2022-10-07 10:20:06 -0700

    sim json files generation

--- routes/sim.js
+++ routes/sim.js
@@ -4,15 +4,70 @@ var router = express.Router();
 
 const fs = require('fs');
 
+var fspecBemsAux = '/opt/bems/bems_gui/test/bems_aux.json'
+var fspecBemsEnv = '/opt/bems/bems_gui/test/bems_env.json'
+
 // -----------------------------------------------------------
 //  GET System Data Paint Page
 // -----------------------------------------------------------
-router.get('/', function(req, res, next) {
+let renderSimPage = function(req, res, next) {
 
-  let envTbl = {Smoke_Machine:true,Smoke_Battery:null,Smoke_EE:null,
-                Fire_Machine:null,Fire_EE:true,Fire_Battery_1:true,Fire_Battery_2:true }
+  let rtnObj = {}
 
-    res.render('sim',{envTbl:envTbl})
+  try {
+    let auxChkStr = fs.readFileSync(fspecBemsAux, {encoding:'utf8', flag:'r'})
+    let envChkStr = fs.readFileSync(fspecBemsEnv, {encoding:'utf8', flag:'r'})
+
+    auxChk = JSON.parse(auxChkStr)
+    envChk = JSON.parse(envChkStr)
+
+    rtnObj.checked = {...auxChk, ...envChk}
+  } catch(err) {
+    console.error(err)
+    rtnObj.checked = {}
+  }
+ 
+  rtnObj.errWd7 = ["Smoke_Machine", "Smoke_Battery","Fire_Machine","Fire_EE","Fire_Battery_1","Fire_Battery_2"]
+
+  rtnObj.errWd8 = ["Temperature_Machine_1_Alarm","Temperature_Machine_1_Fault","Temperature_Machine_2_Alarm",
+  "Temperature_Machine_2_Fault","Temperature_EE_Alarm","Temperature_EE_Fault"]
+
+  rtnObj.errWd5 = ['String_High_Voltage_Aux_Alarm','String_High_Voltage_Aux_Fault','String_Low_Voltage_Aux_Alarm',
+  'String_Low_Voltage_Aux_Fault','String_High_Current_Aux_Alarm','String_High_Current_Aux_Fault',
+  'String_High_Temperature_Aux_Alarm','String_High_Temperature_Aux_Fault','Cell_Low_Voltage_Aux_Alarm',
+  'Cell_Low_Voltage_Aux_Fault']
+ 
+
+  res.render('sim',rtnObj)
+}
+
+// -----------------------------------------------------------
+//  
+// -----------------------------------------------------------
+router.get('/', renderSimPage)
+
+// -----------------------------------------------------------
+//  
+// -----------------------------------------------------------
+router.post('/submit_env', function(req, res, next) {
+
+  let jsonStr = JSON.stringify(req.body);
+  fs.writeFileSync(fspecBemsEnv, jsonStr)
+  renderSimPage(req,res,next)
+  //res.send(req.body) 
+
+})
+
+// -----------------------------------------------------------
+//  
+// -----------------------------------------------------------
+router.post('/submit_aux', function(req, res, next) {
+
+  let jsonStr = JSON.stringify(req.body);
+  fs.writeFileSync(fspecBemsAux, jsonStr)
+  renderSimPage(req,res,next)
+  //res.send(req.body) 
+
 })
 
 // -----------------------------------------------------------
